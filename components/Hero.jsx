a22045c48d905beb7ea1f553f4e739d64a44517b
Nilanchal <nilanchalpanda2003@gmail.com>
2024-10-24 10:40:06 +0530

    Changes get in touch to register in Navbar

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -66,7 +66,7 @@ const Navbar = () => {
                 href="#"
                 className="flex items-center justify-center gap-x-3 border-b bg-transparent text-gray-700 hover:border-b-gray-900 hover:text-gray-900 dark:border-purple-300 dark:text-gray-300 dark:hover:border-b-white dark:hover:text-white"
               >
-                Get in touch
+                Register
                 <span>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
