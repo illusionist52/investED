464acc6ab499c7ff2d82d1dbd3846693372e067f
usmaan <2021.mohdusmaan.mogal@ves.ac.in>
2024-10-24 14:39:54 +0530

    Testing Llama

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+"use client"
+import { Test } from "@/api/llama/test";
 import CtaSection from "@/components/CTA";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/Hero";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
+  useEffect(() => {
+    Test(); // Automatically runs when component mounts
+  }, []);  return (
     <div className="bg-slate-200">
       <HeroSection />
       <CtaSection />
---
+++ public/data/mutualFundsReturns.js
@@ -0,0 +1,206 @@
+// mutualFundReturns.js
+
+const mutualFundReturns = [
+  {
+    "Scheme Name": "Aditya Birla Sun Life India GenNext Fund - Direct Plan - Growth",
+    "Plan": "Direct Plan",
+    "2024": -0.16002312,
+    "2025": 0.6338628,
+    "2026": 0.006570633,
+    "2027": -0.059980243,
+    "2028": 0.795612,
+    "Total Predicted Return": 1.21604207
+  },
+  {
+    "Scheme Name": "Quant Large and Mid Cap Fund - Direct Plan - Growth",
+    "Plan": "Direct Plan",
+    "2024": -0.090250276,
+    "2025": 0.09633397,
+    "2026": 0.123478726,
+    "2027": 0.18738423,
+    "2028": 0.8463968,
+    "Total Predicted Return": 1.16334345
+  },
+  {
+    "Scheme Name": "Quant Large and Mid Cap Fund - Growth",
+    "Plan": "Regular",
+    "2024": -0.110092364,
+    "2025": 0.06483955,
+    "2026": 0.093995295,
+    "2027": 0.17513314,
+    "2028": 0.93533224,
+    "Total Predicted Return": 1.159207861
+  },
+  {
+    "Scheme Name": "Quant ELSS Tax Saver Fund - Growth",
+    "Plan": "Regular",
+    "2024": 0.16561474,
+    "2025": 0.26074913,
+    "2026": 0.2064425,
+    "2027": 0.15702702,
+    "2028": 0.35646686,
+    "Total Predicted Return": 1.14630025
+  },
+  {
+    "Scheme Name": "Quant ELSS Tax Saver Fund - Direct Plan - Growth",
+    "Plan": "Direct Plan",
+    "2024": 0.19516881,
+    "2025": 0.25205955,
+    "2026": 0.214591,
+    "2027": 0.18240702,
+    "2028": 0.2953857,
+    "Total Predicted Return": 1.13961208
+  },
+  {
+    "Scheme Name": "Aditya Birla Sun Life Digital India Fund - Regular Plan - Growth",
+    "Plan": "Regular",
+    "2024": 0.33921608,
+    "2025": 0.36219093,
+    "2026": 0.22449446,
+    "2027": 0.12074798,
+    "2028": 0.058768354,
+    "Total Predicted Return": 1.105417804
+  },
+  {
+    "Scheme Name": "JM ELSS Tax Saver Fund - Growth",
+    "Plan": "Regular",
+    "2024": -0.121552214,
+    "2025": 0.5867072,
+    "2026": -0.02767913,
+    "2027": -0.13501154,
+    "2028": 0.7996153,
+    "Total Predicted Return": 1.102079616
+  },
+  {
+    "Scheme Name": "Quant Focused Fund - Growth",
+    "Plan": "Regular",
+    "2024": -0.09927092,
+    "2025": 0.09429539,
+    "2026": -0.023172794,
+    "2027": 0.02772997,
+    "2028": 1.046456,
+    "Total Predicted Return": 1.046037646
+  },
+  {
+    "Scheme Name": "Quant Focused Fund - Direct Plan - Growth",
+    "Plan": "Direct Plan",
+    "2024": -0.080979936,
+    "2025": 0.13726236,
+    "2026": 0.04291336,
+    "2027": 0.08800074,
+    "2028": 0.84258,
+    "Total Predicted Return": 1.029776524
+  },
+  {
+    "Scheme Name": "Bank of India Manufacturing & Infrastructure Fund - Direct Plan - Growth",
+    "Plan": "Direct Plan",
+    "2024": 0.075490884,
+    "2025": 0.33799174,
+    "2026": 0.19602717,
+    "2027": 0.09744729,
+    "2028": 0.32268554,
+    "Total Predicted Return": 1.029642624
+  },
+  {
+    "Scheme Name": "JM Value Fund - Growth",
+    "Plan": "Regular",
+    "2024": 0.01696905,
+    "2025": 0.35440087,
+    "2026": 0.15765114,
+    "2027": 0.05268666,
+    "2028": 0.42119473,
+    "Total Predicted Return": 1.00290245
+  },
+  {
+    "Scheme Name": "Edelweiss Flexi Cap Fund - Direct Plan - Growth",
+    "Plan": "Direct Plan",
+    "2024": 0.24395984,
+    "2025": 0.20780988,
+    "2026": 0.08629175,
+    "2027": 0.2767626,
+    "2028": 0.18369614,
+    "Total Predicted Return": 0.99852021
+  },
+  {
+    "Scheme Name": "Nippon India Value Fund - Direct Plan - Growth",
+    "Plan": "Direct Plan",
+    "2024": 0.017779237,
+    "2025": 0.43019307,
+    "2026": 0.018130973,
+    "2027": 0.024657471,
+    "2028": 0.4960492,
+    "Total Predicted Return": 0.986809951
+  },
+  {
+    "Scheme Name": "Nippon India Growth Fund - Direct Plan - Growth",
+    "Plan": "Direct Plan",
+    "2024": 0.07044602,
+    "2025": 0.2896826,
+    "2026": 0.16256967,
+    "2027": 0.07977212,
+    "2028": 0.36546138,
+    "Total Predicted Return": 0.96793179
+  },
+  {
+    "Scheme Name": "Sundaram Consumption Fund - Growth",
+    "Plan": "Regular",
+    "2024": -0.3863694,
+    "2025": 0.021232696,
+    "2026": 0.23224626,
+    "2027": 0.14571834,
+    "2028": 0.95463717,
+    "Total Predicted Return": 0.967465066
+  },
+  {
+    "Scheme Name": "DSP Natural Resources and New Energy Fund - Direct Plan - Growth",
+    "Plan": "Direct Plan",
+    "2024": 0.10470628,
+    "2025": 0.20259862,
+    "2026": 0.2323264,
+    "2027": 0.21467504,
+    "2028": 0.19860207,
+    "Total Predicted Return": 0.95290841
+  },
+  {
+    "Scheme Name": "Kotak Infrastructure and Economic Reform Fund - Direct Plan - Growth",
+    "Plan": "Direct Plan",
+    "2024": 0.068955876,
+    "2025": 0.20671737,
+    "2026": 0.17110218,
+    "2027": 0.09513419,
+    "2028": 0.40656823,
+    "Total Predicted Return": 0.948477846
+  },
+  {
+    "Scheme Name": "Bank of India ELSS Tax Saver - Direct Plan - Growth",
+    "Plan": "Direct Plan",
+    "2024": -0.09545435,
+    "2025": 0.45422158,
+    "2026": 0.20339246,
+    "2027": 0.0060617207,
+    "2028": 0.3794805,
+    "Total Predicted Return": 0.9477019107
+  },
+  {
+    "Scheme Name": "Sundaram Consumption Fund - Direct Plan - Growth",
+    "Plan": "Direct Plan",
+    "2024": -0.4018032,
+    "2025": 0.0040910877,
+    "2026": 0.23326665,
+    "2027": 0.16792203,
+    "2028": 0.9257724,
+    "Total Predicted Return": 0.9292489677
+  },
+  {
+    "Scheme Name": "DSP Natural Resources and New Energy Fund - Regular Plan - Growth",
+    "Plan": "Regular",
+    "2024": 0.104600154,
+    "2025": 0.18815677,
+    "2026": 0.24903296,
+    "2027": 0.20000029,
+    "2028": 0.15567791,
+    "Total Predicted Return": 0.897468087
+  }
+];
+
+export default mutualFundReturns;
